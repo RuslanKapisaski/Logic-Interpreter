a4e375cdac3002559682dcd7f8a2eec7e3c814e2
Руслан Каписъски <ruslank1@abv.bg>
2026-01-13 16:14:32 +0200

    Implement Node class
    Add expression tree node evaluation

---
+++ LogicInterpreter/LogicInterpreter/Core/Node.cs
@@ -0,0 +1,44 @@
+﻿namespace LogicInterpreter.Core
+{
+
+    class Node
+    {
+        public NodeType Type;
+        public Node Left;
+        public Node Right;
+
+        public char Name;
+        public int Value;
+
+        public int Evaluate()
+        {
+
+            if (Type == NodeType.Variable)
+            {
+                return Value;
+            }
+
+            if (Type == NodeType.Not)
+            {
+                Value = Left.Evaluate() == 1 ? 0 : 1;
+                return Value;
+            }
+
+            if (Type == NodeType.And)
+            {
+                Value = Left.Evaluate() & Right.Evaluate();
+                return Value;
+            }
+
+            if (Type == NodeType.Or)
+            {
+                Value = Left.Evaluate() | Right.Evaluate();
+                return Value;
+            }
+
+            return 0;
+        }
+    }
+
+    }
+}
