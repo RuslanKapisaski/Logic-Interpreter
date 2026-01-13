5d03f54d23eda5d7991c5789a99697a349719233
Руслан Каписъски <ruslank1@abv.bg>
2026-01-13 15:40:16 +0200

    Create Node type in Core folder
    Add some basic project structure. This Core folder will store node, the type of node and logical function logic

---
+++ LogicInterpreter/LogicInterpreter/Core/NodeType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicInterpreter.Core
+{
+    enum NodeType
+    { 
+        Variable,
+        And,
+        Or,
+        Not
+    }
+
+}
