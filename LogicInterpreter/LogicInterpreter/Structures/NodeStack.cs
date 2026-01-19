fc73acb6c9a36f68151068f20fc93ec2076112c0
Руслан Каписъски <ruslank1@abv.bg>
2026-01-19 16:26:35 +0200

    Implement NodeStack
    Implement NodeStack with push and pop operations

---
+++ LogicInterpreter/LogicInterpreter/Structures/NodeStack.cs
@@ -0,0 +1,25 @@
+﻿using LogicInterpreter.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicInterpreter.Structures
+{
+    class NodeStack
+    {
+        private Node[] data = new Node[100];
+        private int top = -1;
+
+        public void Push(Node node)
+        {
+            data[++top] = node;
+        }
+
+        public Node Pop()
+        {
+            return data[top--];
+        }
+    }
+}
