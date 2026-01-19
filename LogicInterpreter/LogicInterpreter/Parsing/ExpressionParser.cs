be88f6f237785f8e3942592b50ea44f3a68b2434
Руслан Каписъски <ruslank1@abv.bg>
2026-01-19 19:23:28 +0200

    Implement ExpressionParser
    Add stack-based parser for logical expressions with AND, OR, NOT

---
+++ LogicInterpreter/LogicInterpreter/Parsing/ExpressionParser.cs
@@ -0,0 +1,52 @@
+﻿using LogicInterpreter.Core;
+using LogicInterpreter.Structures;
+
+namespace LogicInterpreter.Parsing
+{
+    class ExpressionParser
+    {
+        public Node Parse(string expression)
+        {
+            NodeStack stack = new NodeStack();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char token = expression[i];
+
+                if (token == ' ') continue;
+
+                if (token >= 'a' && token <= 'z')
+                {
+                    stack.Push(new Node
+                    {
+                        Type = NodeType.Variable,
+                        Name = token
+                    });
+                }
+                else if (token == '!')
+                {
+                    Node child = stack.Pop();
+                    stack.Push(new Node
+                    {
+                        Type = NodeType.Not,
+                        Left = child
+                    });
+                }
+                else if (token == '&' || token == '|')
+                {
+                    Node right = stack.Pop();
+                    Node left = stack.Pop();
+
+                    stack.Push(new Node
+                    {
+                        Type = token == '&' ? NodeType.And : NodeType.Or,
+                        Left = left,
+                        Right = right
+                    });
+                }
+            }
+
+            return stack.Pop();
+        }
+    }
+}
