81955e44d03bc411db70d7a1d70d7f92dd947c59
Руслан Каписъски <ruslank1@abv.bg>
2026-01-24 15:48:30 +0200

    Create Solver command

---
+++ LogicInterpreter/LogicInterpreter/Commands/Solver.cs
@@ -0,0 +1,77 @@
+﻿using LogicInterpreter.Core;
+using System;
+
+namespace LogicInterpreter.Commands
+{
+    class Solver
+    {
+        public static void Execute(LogicalFunction function, int[] values)
+        {
+            for (int i = 0; i < function.Parameters.Length; i++)
+            {
+                UpdateVariable(function.Root, function.Parameters[i], values[i]);
+            }
+
+            int result = Evaluate(function.Root);
+            Console.WriteLine("Result: " + result);
+        }
+
+        internal static int Evaluate(Node node)
+        {
+            if (node.IsCalculated)
+            {
+                return node.Value;
+            }
+
+            if (node.Type == NodeType.Variable)
+            {
+                node.IsCalculated = true;
+                return node.Value;
+            }
+
+            if (node.Type == NodeType.Not)
+            {
+                node.Value = Evaluate(node.Left) == 1 ? 0 : 1;
+            }
+            else if (node.Type == NodeType.And)
+            {
+                node.Value = Evaluate(node.Left) & Evaluate(node.Right);
+            }
+
+            else if (node.Type == NodeType.Or)
+            {
+                node.Value = Evaluate(node.Left) | Evaluate(node.Right);
+            }
+
+            node.IsCalculated = true;
+            return node.Value;
+        }
+
+        internal static bool UpdateVariable(Node node, char varName, int newValue)
+        {
+            if (node == null) return false;
+
+            if (node.Type == NodeType.Variable && node.Name == varName)
+            {
+                if (node.Value != newValue)
+                {
+                    node.Value = newValue;
+                    node.IsCalculated = false;
+                    return true;
+                }
+                return false;
+            }
+
+            bool leftChanged = UpdateVariable(node.Left, varName, newValue);
+            bool rightChanged = UpdateVariable(node.Right, varName, newValue);
+
+            if (leftChanged || rightChanged)
+            {
+                node.IsCalculated = false;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
