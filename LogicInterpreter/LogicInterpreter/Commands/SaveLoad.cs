23338eab90babfc8b4cd2d27e3093b5e0ee6b1b0
Руслан Каписъски <ruslank1@abv.bg>
2026-01-25 10:33:43 +0200

    Implement SaveLoad commands
    Save command => creates a file with our defined functions
    Load command => loads a created file with defined functions

---
+++ LogicInterpreter/LogicInterpreter/Commands/SaveLoad.cs
@@ -0,0 +1,30 @@
+﻿using LogicInterpreter.Core;
+using System.IO;
+
+namespace LogicInterpreter.Commands
+{
+    class SaveLoad
+    {
+        public static void Save(string path, string[] defines, int count)
+        {
+            using (StreamWriter w = new StreamWriter(path))
+            {
+                for (int i = 0; i < count; i++)
+                    w.WriteLine(defines[i]);
+            }
+        }
+
+        public static void Load(string path, FunctionCollection functions)
+        {
+            using (StreamReader r = new StreamReader(path))
+            {
+                while (!r.EndOfStream)
+                {
+                    string line = r.ReadLine();
+                    if (line != "")
+                        Define.Execute(line, functions);
+                }
+            }
+        }
+    }
+}
